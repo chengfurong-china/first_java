446051c59ca2a00fddda9d000f8429af59310352
chengfurong-china <cfr_95@163.com>
2015-07-02 16:04:49 +0800

    second commit test

--- src/com/chengfurong/test/TestJava.java
+++ src/com/chengfurong/test/TestJava.java
@@ -7,7 +7,8 @@ public class TestJava {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GitHub Test!");
+		System.out.println("GitHub Test!Good!");
+		
 	}
 
 }
